91df424d6bbce9d56b5d57a3c078d08449ad8010
Sidharth Shanker <sshanker220@gmail.com>
2011-06-29 00:31:09 -0400

    Added categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -42,12 +42,13 @@ class CategoriesController < ApplicationController
       @categories = ['Fraternities', 'Theater', 'Sports', 'Politics', 'Career Networking', 'Arts', 'Community Service', 'Student Council']
     end
     
-    def commmunityservice
+    def communityservice
       @title = "Community Service"
       @array_of_events = compile_categories(7)
       @times = ['12:00 AM', '1:00 AM','2:00 AM', '3:00 AM','4:00 AM','5:00 AM', '6:00 AM','7:00 AM','8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM', '11:00 PM']
       @categories = ['Fraternities', 'Theater', 'Sports', 'Politics', 'Career Networking', 'Arts', 'Community Service', 'Student Council']
     end
+
     
     def studentcouncil
       @title = "Student Council"
