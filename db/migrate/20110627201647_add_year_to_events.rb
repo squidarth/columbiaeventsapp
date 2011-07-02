0e93f65729e71dcca92587c31a8db5adcb7983c2
Sidharth Shanker <sshanker220@gmail.com>
2011-07-02 16:51:19 -0400

    Get rid of spare files

--- db/migrate/20110627201647_add_year_to_events.rb
+++
@@ -1,9 +0,0 @@
-class AddYearToEvents < ActiveRecord::Migration
-  def self.up
-    add_column :events, :year, :string
-  end
-
-  def self.down
-    remove_column :events, :year
-  end
-end
--- db/migrate/20110627201721_add_month_to_events.rb
+++
@@ -1,9 +0,0 @@
-class AddMonthToEvents < ActiveRecord::Migration
-  def self.up
-    add_column :events, :month, :string
-  end
-
-  def self.down
-    remove_column :events, :month
-  end
-end
