47c62fda64b8fbc81e1a22cd906a64392734777f
Sidharth Shanker <sshanker220@gmail.com>
2011-07-29 00:43:23 -0400

    test

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -4,10 +4,10 @@ class AuthorizationsController < ApplicationController
   end
   
   def create
-    auth = request.env["omniauth.auth"]
-    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
-    flash[:notice] = "Authentication successful."
-    redirect_to authentications_url
+    render :text => request.env["omniauth.auth"]
+    #current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
+    #flash[:notice] = "Authentication successful."
+    #redirect_to authentications_url
   end
   
   def destroy
