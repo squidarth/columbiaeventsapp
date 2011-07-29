729a7f551b99e0b62db9b3b6781e8a2caa4a7307
Sidharth Shanker <sshanker220@gmail.com>
2011-07-28 23:56:22 -0400

    added keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'APP_ID', 'APP_SECRET'
+  provider :facebook, '263515600329607', '2649d032a1ca0e33122e3614184ad18c'
 end
