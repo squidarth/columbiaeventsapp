6c0ca6f57b6e379bc9d408f40c078be0b455bb9f
Sidharth Shanker <sshanker220@gmail.com>
2011-07-29 02:03:02 -0400

    test

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -4,15 +4,26 @@ class AuthorizationsController < ApplicationController
   end
   
   def create
-    auth  = request.env["omniauth.auth"]
-    authorization = Authorization.find_by_provider_and_uid(auth['provider'], auth['uid'])
-    if authorization #case that an authorizaiton is found, sign in user
-      sign_in(authorization.user)
-      redirect_to(authorization.user)
-    else
-      current_user.authorizations.create(:provider => auth['provider'], :uid => auth['uid'])
-      redirect_to current_user
-    end
+   render :text => request.env["omniauth.auth"]
+   #auth  = request.env["omniauth.auth"]
+    #authorization = Authorization.find_by_provider_and_uid(auth['provider'], auth['uid'])
+ #   if authorization #case that an authorizaiton is found, sign in user
+  #    sign_in(authorization.user)
+   #   redirect_to(authorization.user)
+   # elsif current_user
+    #  current_user.authorizations.create!(:provider => auth['provider'], :uid => auth['uid'])
+     # redirect_to current_user
+    #else
+     # user = User.new
+      #user.authorizations.build(:provider => auth['provider'], :uid => auth['uid'])
+   #   if user.save!
+    #    sign_in user
+     #   redirect_to user
+    #  else
+     #   session[:auth]  = auth.except('extra')
+      #  redirect_to sign_up
+     # end
+  #  end
 
   end
   
