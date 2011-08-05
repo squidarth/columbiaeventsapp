b170e6d1acc6075f8649feccfac84ae9a41d7ad4
Sidharth Shanker <sshanker220@gmail.com>
2011-08-04 22:28:51 -0400

    changed omniauth permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '263515600329607', '2649d032a1ca0e33122e3614184ad18c'
+  provider :facebook, '263515600329607', '2649d032a1ca0e33122e3614184ad18c', {:scope => 'create_event'}
 end
