7becdbe326df4b91ed8e24fdfd15071225e33c47
Sidharth Shanker <sshanker220@gmail.com>
2011-08-17 12:39:38 -0400

    fb event attempt 12

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -66,7 +66,7 @@ class EventsController < ApplicationController
           end
           @graph = Koala::Facebook::GraphAPI.new(@token)  
           require 'open-uri'
-          picture = Koala::UploadableIO.new(open('http://s3.amazonaws.com/ColumbiaEventsApp/photos/' + @event.id.to_s + '/small.jpeg').path, 'image')
+          picture = Koala::UploadableIO.new(open('http://s3.amazonaws.com/ColumbiaEventsApp/photos/' + @event.id.to_s + '/small.jpeg').string, 'image')
           params = {
               :picture => picture,
               :name => @event.name,
