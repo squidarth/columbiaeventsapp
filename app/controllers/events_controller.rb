4af0fefb5423424626addadf3cc0248811ef63c3
Sidharth Shanker <sshanker220@gmail.com>
2011-08-17 11:58:19 -0400

    attempt 4

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -66,7 +66,7 @@ class EventsController < ApplicationController
           end
           @graph = Koala::Facebook::GraphAPI.new(@token)  
           require 'open-uri'
-          picture = Koala::UploadableIO.new(File.open(open(@event.photo.url)))
+          picture = Koala::UploadableIO.new(File.open(@event.photo.url))
           params = {
               :picture => picture,
               :name => @event.name,
