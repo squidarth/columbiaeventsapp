d44fd0b8ec9fca781477a7f36b25bc8067689f4a
Sidharth Shanker <sshanker220@gmail.com>
2011-08-26 23:09:18 -0400

    Fixed layout again

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-	  $(document).ready(function() {
-    		$("#event_date").datepicker({dateFormat: 'dd-mm-yy'});
-    		$('#event_time').timepicker({
-    			showPeriod: true,
-    			showLeadingZero: true
-    		});
-  	  });
+  $(document).ready(function() {
+		$("#event_date").datepicker({dateFormat: 'dd-mm-yy'});
+		$('#event_time').timepicker({
+			showPeriod: true,
+			showLeadingZero: true
+		});
+  });
