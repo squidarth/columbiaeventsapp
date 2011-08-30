2a5dba85193bb84873557a992fcc66c3efd35246
Sidharth Shanker <sshanker220@gmail.com>
2011-08-30 16:25:26 -0400

    Testing addiiton of making existing events facebook events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -156,47 +156,79 @@ class EventsController < ApplicationController
       #Changes that need to be made
       # 1.) Make sure that the Facebook event is changed as well
       # 2.) (Update all attributes of the field)
-      if @event.update_attributes(params[:event])
-        #include facebook event stuff here
-        #if(@event.facebookevent == 1)
-        #  if(!current_user.authorizations.empty?)
-        #    current_user.authorizations.each do |authorization|
-        #    if authorization.provider.eql?("facebook")
-        #      @token = authorization.token
-        #    end
-       #   end
-      #    graph = Koala::Facebook::GraphAPI.new(@token)
-      #    if(params[:event][:name])
-     #       graph.put_object(@event.facebooklink, '', :name => @event.name)
-     #     end
-    #      if(params[:event][:description])
-    #        graph.put_object(@event.facebooklink, '', :description => params[:event][:description])
-   #       end
-      #    if(params[:event][:location])
-     #       graph.put_object(@event.facebooklink, '', :location => params[:event][:location])
-      #    end
+      if(@event.facebookevent == 0 || @event.facebookevent.nil?)
+        if @event.update_attributes(params[:event])
+          #how do i check that an event has been turned into a Facebook event?
           
-     #     if(params[:event][:date] && params[:event][:time])
-     #       datetime = Time.mktime(@event.date.year, @event.date.month, @event.date.day, @event.time.hour, @event.time.min)
-     ##       graph.put_object(@event.facebooklink, '', :start_time => datetime)
-     #Q     end  
+          if(@event.facebookevent == 1) #CASE THAT EVENT HAS BEEN MADE FACEBOOK EVENT
+            #SAVE THE EVENT AS A FACEBOOK EVENT
+                   if(!current_user.authorizations.empty?)
+                      current_user.authorizations.each do |authorization|
+                        if authorization.provider.eql?("facebook")
+                          @token = authorization.token
+                        end
+                      end
+                      @graph = Koala::Facebook::GraphAPI.new(@token)  
+                      require 'open-uri'
+                      OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
+                      OpenURI::Buffer.const_set 'StringMax', 0
+                      #NEED TO HAVE SEPARATE SEND OPTIONS FOR PICTURE, NO PICTURE, DATE&TIME, NO DATE&TIME
+                      
+                      #CASE THAT PICTURE, DATE, TIME EXIST
+                      if(!@event.photo.nil? && @event.date && @event.time)
+                        picture = Koala::UploadableIO.new(open(@event.photo.url(:small)).path, 'image')
+                        datetime = Time.mktime(@event.date.year, @event.date.month, @event.date.day, @event.time.hour, @event.time.min)
+                        params = {
+                          :picture => picture,
+                          :name => @event.name,
+                          :description => @event.description,
+                          :location => @event.location,
+                          :start_time => datetime,
+                         }
+                      elsif(!@event.photo.nil?) #CASE THAT PHOTO EXISTS, NO DATETIME
+                        picture = Koala::UploadableIO.new(open(@event.photo.url(:small)).path, 'image')
+                        params = {
+                          :picture => picture,
+                          :name => @event.name,
+                          :description => @event.description,
+                          :location => @event.location,
+                         }
+                      elsif(@event.date && @event.time)
+                        datetime = Time.mktime(@event.date.year, @event.date.month, @event.date.day, @event.time.hour, @event.time.min)
+                        params = {
+                          :name => @event.name,
+                          :description => @event.description,
+                          :location => @event.location,
+                          :start_time => datetime,
+                         }
+                      else
+                        params = {
+                          :name => @event.name,
+                          :description => @event.description,
+                          :location => @event.location,
+                         }
+                      end
+                      @event.facebooklink  = @graph.put_object('me', 'events', params )[:id]
+                      @event.save
+                    end
+          end
+          format.html { redirect_to(@event, :notice => 'Event was successfully updated.') }
+          format.xml { head :ok }
           
-    #      if(params[:event][:avatar])
-     #         require 'open-uri'
-     #         OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
-     ##         OpenURI::Buffer.const_set 'StringMax', 0          
-     #         picture = Koala::UploadableIO.new(open(@event.photo.url(:small)).path, 'image')
-     #         graph.put_object(@event.facebooklink, '', :picture => picture)
-      ##    end
-      #   end  
-        #end
-        format.html { redirect_to(@event, :notice => 'Event was successfully updated.') }
-        format.xml { head :ok }
-        
+        else
+          format.html { render :action => "edit" }
+          format.xml { render :xml => @event.errors, :status => :unprocessable_entity }
+        end
       else
-        format.html { render :action => "edit" }
-        format.xml { render :xml => @event.errors, :status => :unprocessable_entity }
+        if @event.update_attributes(params[:event])
+          format.html { redirect_to(@event, :notice => 'Event successfully updated.')}
+          format.xml {head :ok}
+        else
+          format.html { render :action => "edit" }
+          format.xml { render :xml => @event.errors, :status => :unprocessable_entity }
+        end
       end
+   
     end
   end
 
