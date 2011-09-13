8dfa67d1bde88f4388ca18584f6f4a57e15c77c1
Sidharth Shanker <sshanker220@gmail.com>
2011-09-13 02:10:10 -0400

    added rsvp event permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '263515600329607', '2649d032a1ca0e33122e3614184ad18c', {:scope => 'email, create_event, offline_access'}
+  provider :facebook, '263515600329607', '2649d032a1ca0e33122e3614184ad18c', {:scope => 'email, create_event, offline_access', 'rsvp_event'}
 end
