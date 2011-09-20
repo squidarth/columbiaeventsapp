c28c5d207b3bc021dd0a8df4d152a2c889395f1c
Sidharth Shanker <sshanker220@gmail.com>
2011-09-19 20:51:11 -0400

    Fixed fatal bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,7 +27,7 @@ class PagesController < ApplicationController
       end
     end
     filtered_events.sort! {|a,b| b.date <=> a.date}
-    events.delete_if{|event| event.date < ((Date.today +3) && event.date >= Date.today )}
+    events.delete_if{|event| (event.date < (Date.today +3)) && (event.date >= Date.today) )}
     events.each do |event|
       if event.date
           filtered_events << event
