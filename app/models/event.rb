49cdd8148c07bf36acd8941420bc40f3c643b39a
Sidharth Shanker <sshanker220@gmail.com>
2011-09-22 15:52:50 -0400

    Fixing time bug

--- app/models/event.rb
+++ app/models/event.rb
@@ -74,8 +74,9 @@ class Event < ActiveRecord::Base
     end
     events.each do |event|
       if(!event_ids.include?(event['id']))
-      @time_to_change = Time.parse(event["start_time"])
-      @time = Time.mktime(2000, 3, 12, ((@time_to_change.hour)), @time_to_change.min) - 28800 #this hack used to offset time differences
+      @time_to_change = Time.parse(event["start_time"])E
+      @time = Time.mktime(2000, 3, 12, ((@time_to_change.hour)), @time_to_change.min) #this hack used to offset time differences
+      @time = @time-28800
       @date = Date.parse(event["start_time"])
        if create(:user_id => @me.id, :facebooklink => event['id'], :name => event['name'], :author => '', :description => event['description'].to_s, :location => event['location'], :time => @time, :date => @date, :category => 9)
        else
