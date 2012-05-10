80b669944d08b3e9902817f64f6f26bfb63a63ee
Daria Jung <djj2115@columbia.edu>
2012-05-10 15:38:55 -0400

    some changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ ActionMailer::Base.smtp_settings = {
   :domain => 'www.eventsalsa.com',
   :port  => 587,
   :user_name  => "info@eventsalsa.com",
-  :password  => 
+  :password  => "Virtus12",
   :authentication  => :plain
 }
 
