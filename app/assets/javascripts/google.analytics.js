57c240c431248a3d27c48ab3e824ca5a871086f2
Kenneth Cheng <kenthesage@gmail.com>
2012-09-08 17:13:37 -0400

    Switch analytics tracker to staging-eventsalsa for testing

--- app/assets/javascripts/google.analytics.js
+++ app/assets/javascripts/google.analytics.js
@@ -1,5 +1,20 @@
+/* eventsalsa.com */
+
+//var _gaq = _gaq || [];
+//_gaq.push(['_setAccount', 'UA-25696864-1']);
+//_gaq.push(['_trackPageview']);
+
+//(function() {
+    //var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    //ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    //var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+//})();
+
+
+/* staging-eventsalsa.com */
+
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-25696864-1']);
+_gaq.push(['_setAccount', 'UA-34675345-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
